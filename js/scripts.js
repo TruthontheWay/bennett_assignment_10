2e6544198ddecc16f71a751cbbdcb3603202da57
TruthontheWay <Bennettr1@ufl.edu>
2023-11-03 20:26:04 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let image1 = document.querySelector('#spinosaurus')
+    image1.onclick = () => {
+        if (image1.getAttribute('src') === 'images/Spinosaurus_life.png')
+        {
+            image1.setAttribute('src','images/799px-Spinosaurus_size_comparison.png');}
+            else {image1.setAttribute('src','images/Spinosaurus_life.png');
+        }
+    };
+
+    let image2 = document.querySelector('#argentinosaurus')
+     image2.onmouseenter = (e) => {image2.setAttribute('src','images/aregentinosaurrus_scale.png');};
+     image2.onmouseleave = (e) => {image2.setAttribute('src','images/argentinosaurus_bw.jpg');};
